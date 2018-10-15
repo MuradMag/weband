0d3cc2d88775983711ca8bf63b329caefe3fa80e
coolemployer <44072896+coolemployer@users.noreply.github.com>
2018-10-16 00:39:00 +0300

    32

--- webapp.bundle.js
+++ webapp.bundle.js
@@ -7617,7 +7617,7 @@
 			} else if (event.target.uuid === this.char.hrm_data.uuid) {
 				let rate = value.readUInt16BE(0);
 				log$1("Test1 (HEX): ",value.toString('hex'));
-				log$1("TEST2 (unsign): ",value.readUInt16BE(1));
+				log$1("TEST2 (unsign): ",value.readUInt16BE(1).toString('hex'));
 				log$1("TEST3 (value): ",value);
 				// log$1("Raw lenght: ",length(value.toString('hex')));
 				this.emit('heart_rate', rate);
@@ -7648,12 +7648,38 @@
 	async function test_all(miband, log) {
 
 		let info = {
-			time:     await miband.getTime(),
+				time:     await miband.getTime(),
 			battery:  await miband.getBatteryInfo(),
 			hw_ver:   await miband.getHwRevision(),
 			sw_ver:   await miband.getSwRevision(),
 			serial:   await miband.getSerial(),
 	};
+
+		log(`HW ver: ${info.hw_ver}  SW ver: ${info.sw_ver}`);
+		info.serial && log(`Serial: ${info.serial}`);
+		log(`Battery: ${info.battery.level}%`);
+		log(`Time: ${info.time.toLocaleString()}`);
+
+		let ped = await miband.getPedometerStats();
+		log('Pedometer:', JSON.stringify(ped));
+
+		log('Notifications demo...');
+		await miband.showNotification('message');
+		await delay(3000);
+		await miband.showNotification('phone');
+		await delay(5000);
+		await miband.showNotification('off');
+
+		log('Tap MiBand button, quick!');
+		miband.on('button', () => log('Tap detected'));
+		try {
+			await miband.waitButton(10000);
+		} catch (e) {
+			log('OK, nevermind ;)');
+		}
+
+		log('Heart Rate Monitor (single-shot)');
+		log('Result:', await miband.hrmRead());
 
 		log('Heart Rate Monitor (continuous for 30 sec)...');
 		miband.on('heart_rate', (rate) => {
