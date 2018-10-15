7e2f5f81c06f4b77ec9c15af34bc42ec4ef67498
coolemployer <44072896+coolemployer@users.noreply.github.com>
2018-10-16 01:42:01 +0400

    32 some logs

--- webapp.bundle.js
+++ webapp.bundle.js
@@ -7588,6 +7588,19 @@
                */
 
 		_handleNotify(event) {
+			
+			
+	    log$1('size1', event.target.byteLength);
+            log$1('size2', event.target.value.byteLength);
+            log$1('size3', event.target.value.buffer.byteLength);
+            log$1('size4', event.byteLength);
+            log$1(event);
+            log$1(event.target);
+            log$1(event.target.value);
+            log$1(event.target.value.buffer);
+			
+			
+			
 			const value = Buffer.from(event.target.value.buffer);
 
 			if (event.target.uuid === this.char.auth.uuid) {
@@ -7648,38 +7661,13 @@
 	async function test_all(miband, log) {
 
 		let info = {
-				time:     await miband.getTime(),
+			time:     await miband.getTime(),
 			battery:  await miband.getBatteryInfo(),
 			hw_ver:   await miband.getHwRevision(),
 			sw_ver:   await miband.getSwRevision(),
 			serial:   await miband.getSerial(),
 	};
 
-		log(`HW ver: ${info.hw_ver}  SW ver: ${info.sw_ver}`);
-		info.serial && log(`Serial: ${info.serial}`);
-		log(`Battery: ${info.battery.level}%`);
-		log(`Time: ${info.time.toLocaleString()}`);
-
-		let ped = await miband.getPedometerStats();
-		log('Pedometer:', JSON.stringify(ped));
-
-		log('Notifications demo...');
-		await miband.showNotification('message');
-		await delay(3000);
-		await miband.showNotification('phone');
-		await delay(5000);
-		await miband.showNotification('off');
-
-		log('Tap MiBand button, quick!');
-		miband.on('button', () => log('Tap detected'));
-		try {
-			await miband.waitButton(10000);
-		} catch (e) {
-			log('OK, nevermind ;)');
-		}
-
-		log('Heart Rate Monitor (single-shot)');
-		log('Result:', await miband.hrmRead());
 
 		log('Heart Rate Monitor (continuous for 30 sec)...');
 		miband.on('heart_rate', (rate) => {
