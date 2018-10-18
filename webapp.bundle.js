d5bb194a4b15a1d5be0729e0859aa180860d5dc9
coolemployer <44072896+coolemployer@users.noreply.github.com>
2018-10-18 22:15:15 +0300

    54

--- webapp.bundle.js
+++ webapp.bundle.js
@@ -7696,7 +7696,7 @@
 
 
 			log$1('sizeOfDataView: ', event.target.value.byteLength);
-			log$1('size: ', event.target.value.buffer.toString('hex'));
+			log$1('size: ', Buffer.from(event.target.value.buffer).toString('hex'));
 			var cursor = new ArrayBufferCursor(event.target.value.buffer);
 			for (; cursor.hasNext();) {
 				log$1(cursor.next());
@@ -7791,7 +7791,7 @@
 	}
 
 	async function test_all(miband, log) {
-		log('commit 53');
+		log('commit 54');
 
 		// let info = {
 		//   time:     await miband.getTime(),
