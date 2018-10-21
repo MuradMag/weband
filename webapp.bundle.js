e5410f09880ae9ae7c298d19732e2344f4fb9e97
coolemployer <44072896+coolemployer@users.noreply.github.com>
2018-10-22 00:56:26 +0300

    62 again

--- webapp.bundle.js
+++ webapp.bundle.js
@@ -7803,7 +7803,7 @@
 	}
 
 	async function test_all(miband, log) {
-		log('commit SPECIAL');
+		log('commit 62');
 
 		// let info = {
 		//   time:     await miband.getTime(),
@@ -7854,10 +7854,9 @@
 		log('RAW data (no decoding)...')
 		miband.rawStart();
 		log("rawstart is finished");
-		await delay(30000);
+		await delay(600000);
 		log("delay is finished");
 		miband.rawStop();
-		// saveAll();
 		log("rawstop is finished");
 		log('Finished.');
 	}
@@ -7875,22 +7874,6 @@
 
 		output.innerHTML += [...arguments].join(' ') + '\n';
 	}
-
-
-	//
-	// function saveAll() {
-	// 	var text = $("#output").val();
-	// 	var filename = "test";
-	// 	var blob = new Blob([text], {type: "text/plain;charset=utf-8"});
-	// 	saveAs(blob, filename+".txt");
-	// });
-
-
-
-
-
-
-
 
 	async function scan() {
 		if (!bluetooth) {
